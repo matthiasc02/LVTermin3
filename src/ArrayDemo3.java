c597eb97731bb8dcd3f8d30c28cf5eabc93b5aa8
matthiasc02 <king.matl.ms@gmail.com>
2015-11-13 16:03:25 +0100

    demo

---
+++ src/ArrayDemo3.java
@@ -0,0 +1,18 @@
+
+public class ArrayDemo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[]numbers={1,2,3,4,5,6,7,8,9,10};
+		int summe=0;
+		
+		for(int i=0;i<numbers.length;i++){
+			summe += numbers[i];
+		}
+		System.out.println("Die Summer der Zahlen= "+summe);
+		
+		
+
+	}
+
+}
